af2cac8fc6d36aa41dc863f4886b846b66abc8a9
officialjwise <officialjwise20@gmail.com>
2025-08-26 19:02:42 +0000

    Allow any origin for CORS (temporary, for development/testing)

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: true,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   });
